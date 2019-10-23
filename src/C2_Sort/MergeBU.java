e1b5719093f3c741c99ad0e29caad6963cc0c1fd
liyi <1020076136@qq.com>
2019-10-23 11:43:56 +0800

    Merge sort bottom-up

--- src/C2_Sort/MergeBU.java
+++ src/C2_Sort/MergeBU.java
@@ -4,18 +4,16 @@ import StdLib.StdOut;
 
 /**
  * @program: Algorithm4th
- * @description: 归并排序，能够保证任意的长度为N的数组的排序时间跟nlog(n) 成正比
- * 主要缺点是所需空间和n成正比
- *
+ * @description: ${description}
  * @author: liyi
- * @create: 2019-10-22 17:40
+ * @create: 2019-10-23 10:57
  */
-public class Merge {
+public class MergeBU {
     // This class should not be instantiated.
-    private Merge() { }
+    private MergeBU() { }
 
-    // stably merge a[lo .. mid] with a[mid+1 ..hi] using aux[lo .. hi]
-    public static void merge(Comparable[] a, Comparable[] aux, int low, int mid, int high){
+    // stably merge a[lo..mid] with a[mid+1..hi] using aux[lo..hi]
+    private static void merge(Comparable[] a, Comparable[] aux, int low, int mid, int high) {
         for(int i = low; i <= high; ++i){
             aux[i] = a[i];
         }
@@ -34,24 +32,21 @@ public class Merge {
      * @param a the array to be sorted
      */
     public static void sort(Comparable[] a) {
-        Comparable[] aux = new Comparable[a.length];
-        sort(a, aux, 0, a.length-1);
+        int length = a.length;
+        Comparable[] aux = new Comparable[length];
+        //len表示归并子数组的长度，1表示，一个一个的归并，归并后的长度为2,2表示两个两个的归并，归并后的长度为4,以此类推
+        for(int len = 1;len < length; len *= 2){
+            for(int i = 0;i <= length - len; i += len + len){
+                //对于数组长度不满足2的x次幂的数组，mid可能会大于high
+                int low = i, mid = low + len-1, high = Math.min(low + len + len -1,length - 1);
+                merge(a, aux, low, mid, high);
+            }
+        }
         assert isSorted(a);
     }
 
-    // mergesort a[lo..hi] using auxiliary array aux[lo..hi]
-    public static void sort(Comparable[] a, Comparable[] aux, int low,int high){
-        if(high <= low) return;
-        int mid = low + (high - low)/2;
-        sort(a, aux, low, mid);
-        assert isSorted(a, low, mid);
-        sort(a, aux, mid + 1, high);
-        assert isSorted(a, mid + 1, high);
-        merge(a, aux, low, mid, high);
-    }
-
-    /***************************************************************************
-     *  Helper sorting function.
+    /***********************************************************************
+     *  Helper sorting functions.
      ***************************************************************************/
 
     // is v < w ?
@@ -59,15 +54,12 @@ public class Merge {
         return v.compareTo(w) < 0;
     }
 
+
     /***************************************************************************
      *  Check if array is sorted - useful for debugging.
      ***************************************************************************/
     private static boolean isSorted(Comparable[] a) {
-        return isSorted(a, 0, a.length - 1);
-    }
-
-    private static boolean isSorted(Comparable[] a, int lo, int hi) {
-        for (int i = lo + 1; i <= hi; i++)
+        for (int i = 1; i < a.length; i++)
             if (less(a[i], a[i-1])) return false;
         return true;
     }
@@ -80,13 +72,13 @@ public class Merge {
     }
 
     /**
-     * Reads in a sequence of strings from standard input; mergesorts them;
-     * and prints them to standard output in ascending order.
+     * Reads in a sequence of strings from standard input; bottom-up
+     * mergesorts them; and prints them to standard output in ascending order.
      *
      * @param args the command-line arguments
      */
     public static void main(String[] args) {
-        String[] a = new String[]{"a","g","y","z","b","c","c","d"};
+        Integer[] a = new Integer[]{4,2,1,5,7,6,3};
         sort(a);
         show(a);
     }
